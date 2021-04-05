2d5f1ce56617831f2a30be90686b82395f62d09b
VladimirGavrilov <gavriloff.v@yandex.ru>
2021-04-05 17:54:44 +0300

    Add validazia registr

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,25 @@
 const {Router} = require('express')
 const bcript = require('bcryptjs')
+const {check, validationResult} = require('express-validator')
 const User = require('../models/User')
 const router = Router()
 
 // /api/auth/register
-router.post('register', async (req, res) => {
-   try{
+router.post('register',
+    [
+        check('email', 'Некорректный email').isEmail(),
+        check('password', 'Минимальная длина пароля 6 символов')
+            .isLength({min: 6})
+    ],
+    async (req, res) => {
+    try{
+       const errors = validationResult(req)
+       if(errors.isEmpty()){
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'Некоректные данные при регистрации'
+            })
+       }
        const {email, password} = req.body
        const candidate = await User.findOne({ email })
        if(candidate) {
@@ -17,7 +31,7 @@ router.post('register', async (req, res) => {
 
        await user.save()
 
-       res.status(201).json({ message:'Пользователь создан'})
+       res.status(201).json({message:'Пользователь создан'})
 
 
 
