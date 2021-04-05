4e959ac63e2538c07f0b5c275948e29773f80298
VladimirGavrilov <gavriloff.v@yandex.ru>
2021-04-05 11:59:02 +0300

    Add config Serrver

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Am Work!')
+ const express = require('express')
+ const config = require('config')
+ const mongoos = require('mongoose')
+ const app = express()
+
+ const PORT = config.get('port') || 5000
+
+ async function start(){
+     try{
+         mongoos.connect(config.get('mongoUri'),)
+     }catch(e){
+         console.log('Server Error', e.message)
+         process.exit(1)
+     }
+ 
+ }
+
+ start()
+ app.listen(PORT, () => console.log(`App has been started ${PORT}...`))
