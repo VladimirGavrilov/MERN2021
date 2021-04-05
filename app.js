5bdc27c625d443c597991f017a059bede0f05dfd
VladimirGavrilov <gavriloff.v@yandex.ru>
2021-04-05 16:46:26 +0300

    Requier DB add Router

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
  const express = require('express')
  const config = require('config')
  const mongoos = require('mongoose')
+//  const routes
  const app = express()
+ app.use('/api/auth', require('./routes/auth.routes'))
 
  const PORT = config.get('port') || 5000
 
  async function start(){
      try{
-         mongoos.connect(config.get('mongoUri'),)
+         mongoos.connect(config.get('mongoUri'),{
+             useNewUrlParser: true,
+             useUnifiedTopology: true,
+             useCreateIndex: true
+         })
      }catch(e){
          console.log('Server Error', e.message)
          process.exit(1)
