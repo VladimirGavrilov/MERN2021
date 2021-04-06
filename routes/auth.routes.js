bc0e6346c0d91224a14cef8992c8d5a2891aba3e
VladimirGavrilov <gavriloff.v@yandex.ru>
2021-04-06 17:55:21 +0300

    Add token

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,61 +1,71 @@
 const {Router} = require('express')
 const bcript = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const {check, validationResult} = require('express-validator')
 const User = require('../models/User')
 const router = Router()
 
 // /api/auth/register
-router.post('/register',
+router.post(
+    '/register',
     [
         check('email', 'Некорректный email').isEmail(),
-        check('password', 'Минимальная длина пароля 6 символов')
-            .isLength({min: 6})
+        check('password', 'Минимальная длина пароля 6 символов').isLength({min: 6})
     ],
     async (req, res) => {
-    try{
-       const errors = validationResult(req)
-       if(errors.isEmpty()){
+        try{
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
             return res.status(400).json({
                 errors: errors.array(),
                 message: 'Некоректные данные при регистрации'
             })
-       }
-       const {email, password} = req.body
-       const candidate = await User.findOne({ email })
-       if(candidate) {
-           return res.status(400).json({message:'Такой пользователь уже существует'})
-       }
+        }
+        const {email, password} = req.body
+        const candidate = await User.findOne({ email })
+        if(candidate) {
+            return res.status(400).json({message:'Такой пользователь уже существует'})
+        }
 
-       const hashedPassword = await bcript.hash(password, 12)
-       const user = new User({email, password: hashedPassword})
+        const hashedPassword = await bcript.hash(password, 12)
+        const user = new User({email, password: hashedPassword})
 
-       await user.save()
+        await user.save()
 
-       res.status(201).json({message:'Пользователь создан'})
+        res.status(201).json({message:'Пользователь создан'})
 
 
 
-   }catch(e) {
-       res.status(500).json({messege: 'Чтото пошло не так...'})
-   }
+    }catch(e) {
+        res.status(500).json({messege: 'Чтото пошло не так...'})
+    }
 })
 
 // /api/auth/login
 router.post('/login',
-            [
-
-
-            ],
+    [
+        check('email', 'Некорректный email').normalizeEmail().isEmail(),
+        check('password', 'Введите пароль').exists()
+    ],
             async (req, res) => {
             try {
                 const errors = validationResult(req)
-                if (errors.isEmpty()) {
+                if (!errors.isEmpty()) {
                     return res.status(400).json({
-                        errors: errors.array(),
-                        message: 'Некоректные данные при входе в систему'
+                    errors: errors.array(),
+                    message: 'Некоректные данные при входе в систему'
                     })
                 }
                 
+                const {email, password} = req.body
+                const user = await User.findOne({email})
+                if(!user){
+                    return res.status(400).json({message: 'Пользователь не найден'})
+                }
+                 const isMatch = await bcript.compare(password, user.password )
+                 if (!isMatch) {
+                     return res.status(500).json({ message: 'Неверный пароль найден' })
+                 }
 
 
 
